96f5f731e5996d8cfc74f402135e7e4e6a17bc31
Matthias Mau <matthias.mau@outlook.com>
2020-05-26 11:27:14 +0300

    commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,24 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  # READ ALL -> DISPLAY ALL TASKS
+  get "tasks", to: "tasks#index"
+
+  # CREATE step 1: GET the form to create new task
+  get "tasks/new", to: "tasks#new", as: :new
+  # CREATE step 2: POST the form
+  post "tasks", to: "tasks#create"
+
+  # READ ONE -> when clicking task title and navigate to a new page, displaying the details of this task
+  get "tasks/:id", to: "tasks#show", as: :task
+
+  # UPDATE/EDIT - Step 1: GET
+  get "tasks/:id/edit", to: "tasks#edit", as: :edit_task
+  # UPDATE/EDIT - Step 2: Patch
+  patch "tasks/:id", to: "tasks#update"
+
+  # DELETE
+  delete "tasks/:id", to: "tasks#destroy", as: :delete_task
+
+  # resources :tasks
 end
